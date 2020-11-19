54e0722e89f07b81558331677b63c93d227b7d62
Abhinav-Mingwal <abhinav.mingwal@gmail.com>
2020-11-19 15:08:57 +0530

    fixed

--- django_project_1/urls.py
+++ django_project_1/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('register', user_views.register,name='register'),
+    url('profile', user_views.profile,name='profile'),
     url('login', auth_views.LoginView.as_view(template_name="users/login.html"),name='login'),
     url('logout', auth_views.LogoutView.as_view(template_name="users/logout.html"),name='logout'),
     url('', include('blog.urls')),
